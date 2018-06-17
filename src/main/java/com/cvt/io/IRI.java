41a0b7b1dfd12f37a88ea8e282486f9563e7ba5a
huanggang5 <huanggang1>
2018-06-18 00:03:28 +0800

    dfgdfg

--- src/main/java/com/cvt/io/IRI.java
+++ src/main/java/com/cvt/io/IRI.java
@@ -25,7 +25,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "CVT";
     public static final String TESTNET_NAME = "CVT Testnet";
-    public static final String VERSION = "1.4.2.4";
+    public static final String VERSION = "1.0.0.0";
     public static CVT cvt;
     public static API api;
     public static IXI ixi;
